7202465b45f7b2ff905a0989cfb67a4e34fd4b67
janvanrijn@gmail.com <janvanrijn@gmail.com>
2017-08-10 16:09:40 +0200

    run model program

---
+++ cmd/run_model.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/home/vanrijn/projects/openml-python:/home/vanrijn/projects/Study-14/Python:/home/vanrijn/projects/openml-pimp
+/home/vanrijn/projects/pythonvirtual/pimp/bin/python3.5 /home/vanrijn/projects/openml-pimp/openmlpimp/generatedata/generate.py --openml_apikey d488d8afd93b32331cf6ea9d7003d4c3 --study_id 14 --classifier random_forest
