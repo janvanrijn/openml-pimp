1a39654e2d6c4090ab3215af990efe6c25e063cb
Jan van Rijn <janvanrijn@gmail.com>
2018-02-09 02:16:39 +0100

    adjusted plotting script

--- openmlpimp/examples/optimizers_vs_defaults.py
+++ openmlpimp/examples/optimizers_vs_defaults.py
@@ -20,6 +20,8 @@ def parse_args():
     parser.add_argument('--measure', type=str, default='predictive_accuracy')
     parser.add_argument('--study_id', type=str, default='OpenML100')
     parser.add_argument('--task_limit', type=int, default=None)
+    parser.add_argument('--setup', type=str, default='hyperband_5')
+    parser.add_argument('--seed', type=int, default=None, help='the seed (efficiency)')
 
     args = parser.parse_args()
     return args
@@ -104,6 +106,7 @@ def plot_scatter(defaults_results, priors_results):
 
 
 def plot_boxplot(priors_results, mode):
+    output_directory = os.path.join(args.output_directory, args.setup)
     all = []
     keys = []
     for idx, classifier in enumerate(priors_results.keys()):
@@ -113,7 +116,10 @@ def plot_boxplot(priors_results, mode):
         draws = 0
         for task_id in priors_results[classifier]:
             if len(priors_results[classifier][task_id]) == 2 and 'uniform' in priors_results[classifier][task_id] and 'kde' in priors_results[classifier][task_id]:
-                current = priors_results[classifier][task_id]['kde'] - priors_results[classifier][task_id]['uniform']
+                scores_kde = priors_results[classifier][task_id]['kde']
+                scores_uniform = priors_results[classifier][task_id]['uniform']
+
+                current = sum(scores_kde.values()) / len(scores_kde) - sum(scores_uniform.values()) / len(scores_kde)
                 data.append(current)
                 if current > 0: kde_wins +=1
                 elif current < 0: uni_wins += 1
@@ -126,14 +132,15 @@ def plot_boxplot(priors_results, mode):
         plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
         plt.plot([0.5, 1.5], [0, 0], 'k-', linestyle='--', lw=1)
         plt.violinplot(data)
-        plt.savefig('/home/vanrijn/experiments/priors_%s_%s.pdf' %(classifier, mode), bbox_inches='tight')
+        plt.savefig(output_directory + '/priors_%s_%s.pdf' %(classifier, mode), bbox_inches='tight')
         plt.close()
 
     plt.figure()
     plt.violinplot(all, list(range(len(all))))
     plt.plot([-0.5, len(all)-0.5], [0, 0], 'k-', linestyle='--', lw=1)
     plt.xticks(list(range(len(keys))), keys, rotation=45, ha='right')
-    plt.savefig('/home/vanrijn/experiments/priors_violin_%s.pdf' %mode, bbox_inches='tight')
+    output_file = output_directory + '/priors_violin_%s.pdf' %mode
+    plt.savefig(output_file, bbox_inches='tight')
     plt.close()
 
 
@@ -144,10 +151,10 @@ if __name__ == '__main__':
     if args.task_limit:
         all_tasks = random.sample(all_tasks, args.task_limit)
 
-    all_classifiers = list_classifiers(args.result_directory)
+    all_classifiers = list_classifiers(args.result_directory + '/' + args.setup)
     print(openmlpimp.utils.get_time(), all_classifiers)
-    priors_results_test = collections.defaultdict(lambda: collections.defaultdict(dict))
-    priors_results_valid = collections.defaultdict(lambda: collections.defaultdict(dict))
+    priors_results_test = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(dict)))
+    priors_results_valid = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(dict)))
     defaults_results = collections.defaultdict(dict)
     for classifier, directory_suffix in all_classifiers:
         for task_id in all_tasks:
@@ -157,25 +164,30 @@ if __name__ == '__main__':
             # if os.path.isfile(default_run_xml):
             #     default_score = get_score_from_xml(default_run_xml, args.measure)
             #     defaults_results[classifier][task_id] = default_score
+            classifier_dir = os.path.join(args.result_directory, args.setup, directory_suffix)
+            for strategy in os.listdir(classifier_dir):
+                task_dir = os.path.join(classifier_dir, strategy, str(task_id))
+                if os.path.isdir(task_dir):
+                    for seed in os.listdir(task_dir):
+                        if args.seed is not None and args.seed is not int(seed):
+                            continue
+                        strategy_name = strategy.split('__')[0]
+                        strategy_trace = classifier_dir + '/' + strategy + '/' + str(task_id) + '/' + seed + '/trace.arff'
+                        strategy_predictions = classifier_dir + '/' + strategy + '/' + str(task_id) + '/' + seed + '/predictions.arff'
 
-            for strategy in os.listdir(args.result_directory + directory_suffix):
-                strategy_name = strategy.split('__')[0]
-                strategy_trace = args.result_directory + directory_suffix + '/' + strategy + '/' + str(task_id) + '/trace.arff'
-                strategy_predictions = args.result_directory + directory_suffix + '/' + strategy + '/' + str(task_id) + '/predictions.arff'
+                        if not os.path.isfile(strategy_predictions) or not os.path.isfile(strategy_trace):
+                            print(openmlpimp.utils.get_time(), '%s: Task %d not finished for strategy %s seed %s' %(classifier, task_id, strategy, seed))
+                            continue
 
-                if not os.path.isfile(strategy_predictions) or not os.path.isfile(strategy_trace):
-                    print(openmlpimp.utils.get_time(), '%s: Task %d not finished for strategy %s' %(classifier, task_id, strategy))
-                    continue
+                        with open(strategy_predictions, 'r') as fp:
+                            predictions_arff = arff.load(fp)
 
-                with open(strategy_predictions, 'r') as fp:
-                    predictions_arff = arff.load(fp)
+                        run = openml.runs.OpenMLRun(flow_id=-1, dataset_id=-1, task_id=task_id)
+                        run.data_content = predictions_arff['data']
+                        score = run.get_metric_fn(sklearn.metrics.accuracy_score)
 
-                run = openml.runs.OpenMLRun(flow_id=-1, dataset_id=-1, task_id=task_id)
-                run.data_content = predictions_arff['data']
-                score = run.get_metric_fn(sklearn.metrics.accuracy_score)
-
-                priors_results_test[classifier][task_id][strategy_name] = score.mean()
-                priors_results_valid[classifier][task_id][strategy_name] = trace_to_score(strategy_trace)
+                        priors_results_test[classifier][task_id][strategy_name][seed] = score.mean()
+                        priors_results_valid[classifier][task_id][strategy_name][seed] = trace_to_score(strategy_trace)
     # plot_scatter(defaults_results, priors_results_test)
     plot_boxplot(priors_results_test, 'test')
     plot_boxplot(priors_results_valid, 'validation')
