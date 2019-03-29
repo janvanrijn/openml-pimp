b556eade40385971a78d79c11961983a59f97213
janvanrijn <janvanrijn@gmail.com>
2019-03-28 20:18:02 -0400

    modularized per dataset

--- examples/experiments/run_pimp_on_arff.py
+++ examples/experiments/run_pimp_on_arff.py
@@ -24,10 +24,11 @@ def read_cmd():
     parser.add_argument('--classifier', default='resnet', type=str)
     parser.add_argument('--config_library', default='openmlpimp', type=str)
     parser.add_argument('--measure', default='predictive_accuracy', type=str)
-    parser.add_argument('--plot_marginals', action='store_true', default=True)
+    parser.add_argument('--plot_marginals', action='store_true', default=False)
     parser.add_argument('--comb_size', default=2, type=int)
     parser.add_argument('--n_trees', default=16, type=int)
     parser.add_argument('--resolution', default=100, type=int)
+    parser.add_argument('--task_id', default=None, type=str)
     parser.add_argument('--task_id_column', default='dataset', type=str)
     parser.add_argument('--subsample', default=None, type=int)
     args_, misc = parser.parse_known_args()
@@ -36,6 +37,7 @@ def read_cmd():
 
 
 def plot_single_marginal(config_space: ConfigSpace.ConfigurationSpace,
+                         name_prefix: str,
                          hyperparameter_idx: int,
                          visualizer: fanova.visualizer.Visualizer,
                          directory: str,
@@ -44,7 +46,7 @@ def plot_single_marginal(config_space: ConfigSpace.ConfigurationSpace,
     plt.clf()
     hyperparameter = config_space.get_hyperparameter_by_idx(hyperparameter_idx)
     os.makedirs(directory, exist_ok=True)
-    outfile_name = os.path.join(directory, hyperparameter.replace(os.sep, "_") + ".pdf")
+    outfile_name = os.path.join(directory, '%s__%s.png' % (name_prefix, hyperparameter.replace(os.sep, "_")))
     visualizer.plot_marginal(hyperparameter_idx, show=False)
 
     x1, x2, _, _ = plt.axis()
@@ -55,6 +57,7 @@ def plot_single_marginal(config_space: ConfigSpace.ConfigurationSpace,
 
 
 def plot_pairwise_marginal(config_space: ConfigSpace.ConfigurationSpace,
+                           name_prefix: str,
                            hyperparameter_idx: typing.Tuple[int],
                            visualizer: fanova.visualizer.Visualizer,
                            directory: str,
@@ -68,9 +71,11 @@ def plot_pairwise_marginal(config_space: ConfigSpace.ConfigurationSpace,
         hp1 = config_space.get_hyperparameter_by_idx(hp1_hp2[0])
         hp2 = config_space.get_hyperparameter_by_idx(hp1_hp2[1])
         os.makedirs(directory, exist_ok=True)
-        outfile_name = os.path.join(directory, hp1.replace(os.sep, "_") + "__" + hp2.replace(os.sep, "_") + ".pdf")
+        outfile_name = os.path.join(directory, '%s__%s__%s.png' % (name_prefix,
+                                                                   hp1.replace(os.sep, "_"),
+                                                                   hp2.replace(os.sep, "_")))
         try:
-            visualizer.plot_pairwise_marginal(hp1_hp2, resolution=20, show=False)
+            visualizer.plot_pairwise_marginal(hp1_hp2, resolution=100, show=False)
 
             ax = plt.gca()
             if z_range:
@@ -117,8 +122,8 @@ def run(args):
     task_ids = data[args.task_id_column].unique()
 
     result = list()
-    for idx, task_id in enumerate(task_ids):
-        logging.info('Running fanova on task %s (%d/%d)' % (task_id, idx + 1, len(task_ids)))
+    for t_idx, task_id in enumerate(task_ids):
+        logging.info('Running fanova on task %s (%d/%d)' % (task_id, t_idx + 1, len(task_ids)))
         data_task = data[data[args.task_id_column] == task_id]
         if args.subsample:
             indices = np.random.choice(len(data_task), args.subsample, replace=False)
@@ -139,29 +144,29 @@ def run(args):
                                            args.output_directory, y_label='Predictive Accuracy')
         indices = list(range(len(config_space.get_hyperparameters())))
         for comb_size in range(1, args.comb_size + 1):
-            for idx in itertools.combinations(indices, comb_size):
-                param_names = np.array(config_space.get_hyperparameter_names())[np.array(idx)]
+            for h_idx in itertools.combinations(indices, comb_size):
+                param_names = np.array(config_space.get_hyperparameter_names())[np.array(h_idx)]
                 logging.info('-- Calculating marginal for %s' % param_names)
-                importance = evaluator.quantify_importance(idx)[idx]
+                importance = evaluator.quantify_importance(h_idx)[h_idx]
                 if comb_size == 1:
-                    visualizer_res = vis.generate_marginal(idx[0], args.resolution)
+                    visualizer_res = vis.generate_marginal(h_idx[0], args.resolution)
                     # visualizer returns mean, std and potentially grid
                     avg_marginal = np.array(visualizer_res[0])
 
                     if args.plot_marginals:
                         plot_single_marginal(
-                            config_space, idx[0], vis,
+                            config_space, task_id, h_idx[0], vis,
                             os.path.join(args.output_directory, str(task_id), 'singular'),
                             None
                         )
                 elif comb_size == 2:
-                    visualizer_res = vis.generate_pairwise_marginal(idx, args.resolution)
+                    visualizer_res = vis.generate_pairwise_marginal(h_idx, args.resolution)
                     # visualizer returns grid names and values
                     avg_marginal = np.array(visualizer_res[1])
 
                     if args.plot_marginals:
                         plot_pairwise_marginal(
-                            config_space, idx, vis,
+                            config_space, task_id, h_idx, vis,
                             os.path.join(args.output_directory, str(task_id), 'pairwise'),
                             None
                         )
@@ -180,7 +185,8 @@ def run(args):
                 
                 result.append(current)
     df_result = pd.DataFrame(result)
-    result_path = os.path.join(args.output_directory, 'fanova_%s_depth_%d.csv' % (args.classifier, args.comb_size))
+    result_path = os.path.join(args.output_directory, 'fanova_%s_depth_%d%s.csv' % (
+        args.classifier, args.comb_size, '_%s' % args.task_id if args.task_id else ''))
     df_result.to_csv(result_path)
     logging.info('resulting csv: %s' % result_path)
     logging.info('To plot, run <openml_pimp_root>/examples/plot/plot_fanova.py')
